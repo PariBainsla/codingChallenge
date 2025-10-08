494ca92eac747f2811ab3172c3dd4feed897cda5
PariBainsla <paribainsla161@gmail.com>
2025-10-08 16:40:24 -0700

    firstNonRepeated solved

---
+++ 3_nonRepeated.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+char firstNonRepeated(string s){
+    int n = s.length();
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(s[i] == s[j])
+            count++;
+        }
+        if(count ==1)
+        return s[i];
+    }
+    return '/0';
+}
+
+int main(){
+    string str;
+    cout<<"Enter the string: ";
+    cin>>str;
+
+    if(firstNonRepeated(str) != '\0'){
+        cout<< firstNonRepeated(str);
+    }
+    else{
+        cout<< "None";
+    }
+}
