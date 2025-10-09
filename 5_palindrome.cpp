8c1d9a38afec7ef1e32d10993101de073d86d272
PariBainsla <paribainsla161@gmail.com>
2025-10-08 19:09:34 -0700

    Palindrome solved

---
+++ 5_palindrome.cpp
@@ -0,0 +1,34 @@
+// PALINDROME CHECK
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter the word: ";
+    cin >> s;
+
+    int start = 0;
+    int end = s.length() - 1;
+    bool isPalindrome = true;
+
+    while (start < end)
+    {
+        if (s[start] != s[end])
+        {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+    if (isPalindrome)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+}
